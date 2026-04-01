a2aabde2035c2b4e55c60665fb3ff0c1a22518cd
k3ssler <jkessler365@mail.ru>
2026-04-01 14:12:12 +0300

    fixed logical error on matrix generations

--- RacingGuru/internal/core/sudoku/getTeamMatrixUseCase.go
+++ RacingGuru/internal/core/sudoku/getTeamMatrixUseCase.go
@@ -32,11 +32,11 @@ func generateConditionsTeams() (models.MatrixTeams, error) {
 		{Label: "Больше 300 гонок", Field: "total_races", Op: models.ConditionOperatorGT, Value: 300},
 		{Label: "Меньше 100 гонок", Field: "total_races", Op: models.ConditionOperatorLT, Value: 100},
 		{Label: "Хотя бы 1 гонка", Field: "total_races", Op: models.ConditionOperatorGTE, Value: 1},
-		{Label: "Больше 30 побед", Field: "total_wins", Op: models.ConditionOperatorGT, Value: 30},
+		{Label: "Больше 10 побед", Field: "total_wins", Op: models.ConditionOperatorGT, Value: 10},
 		{Label: "Меньше 10 побед", Field: "total_wins", Op: models.ConditionOperatorLT, Value: 10},
 		{Label: "Хотя бы 1 победа", Field: "total_wins", Op: models.ConditionOperatorGTE, Value: 1},
 		{Label: "Без побед", Field: "total_wins", Op: models.ConditionOperatorEQ, Value: 0},
-		{Label: "Больше 75 подиумов", Field: "total_podiums", Op: models.ConditionOperatorGT, Value: 75},
+		{Label: "Больше 10 подиумов", Field: "total_podiums", Op: models.ConditionOperatorGT, Value: 10},
 		{Label: "Меньше 20 подиумов", Field: "total_podiums", Op: models.ConditionOperatorLT, Value: 20},
 		{Label: "Хотя бы 1 подиум", Field: "total_podiums", Op: models.ConditionOperatorGTE, Value: 1},
 		{Label: "Без подиумов", Field: "total_podiums", Op: models.ConditionOperatorEQ, Value: 0},
@@ -44,8 +44,8 @@ func generateConditionsTeams() (models.MatrixTeams, error) {
 		{Label: "Меньше 250 очков", Field: "total_points", Op: models.ConditionOperatorLT, Value: 250},
 		{Label: "Набирала очки", Field: "total_points", Op: models.ConditionOperatorGTE, Value: 1},
 		{Label: "Без очков", Field: "total_points", Op: models.ConditionOperatorEQ, Value: 0},
-		{Label: "Больше 20 поулов", Field: "total_poles", Op: models.ConditionOperatorGT, Value: 20},
-		{Label: "Меньше 5 поулов", Field: "total_poles", Op: models.ConditionOperatorLT, Value: 5},
+		{Label: "Больше 10 поулов", Field: "total_poles", Op: models.ConditionOperatorGT, Value: 10},
+		{Label: "Меньше 10 поулов", Field: "total_poles", Op: models.ConditionOperatorLT, Value: 10},
 		{Label: "Хотя бы 1 поул", Field: "total_poles", Op: models.ConditionOperatorGTE, Value: 1},
 		{Label: "Без поулов", Field: "total_poles", Op: models.ConditionOperatorEQ, Value: 0},
 		{Label: "Лучший финиш: победа", Field: "best_finish", Op: models.ConditionOperatorEQ, Value: 1},
@@ -56,7 +56,7 @@ func generateConditionsTeams() (models.MatrixTeams, error) {
 		{Label: "Лучшая квалификация в топ-3", Field: "best_qualifying", Op: models.ConditionOperatorLTE, Value: 3},
 		{Label: "Лучшая квалификация в топ-10", Field: "best_qualifying", Op: models.ConditionOperatorLTE, Value: 10},
 		{Label: "Лучшая квалификация вне топ-3", Field: "best_qualifying", Op: models.ConditionOperatorGT, Value: 3},
-		{Label: "Лучший результат в чемпионате в топ-3", Field: "best_championship_position", Op: models.ConditionOperatorLTE, Value: 3},
+		{Label: "Лучший результат в чемпионате в топ-5", Field: "best_championship_position", Op: models.ConditionOperatorLTE, Value: 5},
 	}
 
 	if err := fillMatrixConditions(conditions, &matrix.ConditionSpecs); err != nil {
