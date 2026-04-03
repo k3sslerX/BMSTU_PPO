3d4f10581da682789247f409dd760dea4706a78e
k3ssler <jkessler365@mail.ru>
2026-04-03 11:15:36 +0300

    test log added

--- RacingGuru/internal/core/sudoku/getTeamMatrixUseCase_test.go
+++ RacingGuru/internal/core/sudoku/getTeamMatrixUseCase_test.go
@@ -5,14 +5,18 @@ import (
 	"RacingGuru/internal/shared"
 	"context"
 	"errors"
+	"fmt"
+	"strings"
 	"testing"
 )
 
 func TestGetTeamMatrixUseCase(t *testing.T) {
 	var err error
+	var matrix models.MatrixTeams
 
 	testUc1 := NewGetTeamMatrixUseCase(&testRepo{returnError: false}, models.User{Role: models.RoleUser})
-	_, err = testUc1.Run(context.Background())
+	matrix, err = testUc1.Run(context.Background())
+	t.Log(formatTeamConditionMatrix(matrix))
 	if err != nil {
 		t.Error(err)
 	}
@@ -22,4 +26,26 @@ func TestGetTeamMatrixUseCase(t *testing.T) {
 	if !errors.Is(err, shared.ErrorNotFound) {
 		t.Error(err)
 	}
+}
+
+func formatTeamConditionMatrix(matrix models.MatrixTeams) string {
+	var sb strings.Builder
+
+	sb.WriteString("Team condition matrix:\n")
+	sb.WriteString(fmt.Sprintf("      | %-35s | %-35s | %-35s |\n",
+		matrix.ConditionSpecs[0][0].Label,
+		matrix.ConditionSpecs[0][1].Label,
+		matrix.ConditionSpecs[0][2].Label,
+	))
+
+	for i := range matrix.ConditionSpecs[1] {
+		sb.WriteString(fmt.Sprintf("%-5s | %-35s | %-35s | %-35s |\n",
+			matrix.ConditionSpecs[1][i].Label,
+			"x",
+			"x",
+			"x",
+		))
+	}
+
+	return sb.String()
 }
