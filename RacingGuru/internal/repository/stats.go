07b3b086424daf158f96ace8d0def9cc072eb955
k3ssler <jkessler365@mail.ru>
2026-04-08 20:17:33 +0300

    stats bug fixed

--- RacingGuru/internal/repository/stats.go
+++ RacingGuru/internal/repository/stats.go
@@ -3,6 +3,7 @@ package repository
 import (
 	"RacingGuru/internal/models"
 	"context"
+	"time"
 
 	"github.com/google/uuid"
 )
@@ -12,7 +13,7 @@ func (r *Repository) GetDriverStats(ctx context.Context, driver models.Driver) (
 	driverStats.Driver = driver
 	stats := models.Stats{}
 	row := r.Pool.QueryRow(ctx,
-		"SELECT total_races, total_wins, total_podiums, total_points, total_poles "+
+		"SELECT total_races, total_wins, total_podiums, total_points, total_poles, "+
 			"best_finish, best_qualifying, championship_wins, best_championship_position "+
 			"FROM CalculateDriverStats($1)", driver.Id)
 	err := row.Scan(&stats.TotalRaces, &stats.TotalWins, &stats.TotalPodiums, &stats.TotalPoints, &stats.TotalPoles,
@@ -30,7 +31,7 @@ func (r *Repository) GetTeamStats(ctx context.Context, team models.Team) (models
 	teamStats.Team = team
 	stats := models.Stats{}
 	row := r.Pool.QueryRow(ctx,
-		"SELECT total_races, total_wins, total_podiums, total_points, total_poles "+
+		"SELECT total_races, total_wins, total_podiums, total_points, total_poles, "+
 			"best_finish, best_qualifying, championship_wins, best_championship_position "+
 			"FROM CalculateTeamStats($1)", team.Id)
 	err := row.Scan(&stats.TotalRaces, &stats.TotalWins, &stats.TotalPodiums, &stats.TotalPoints, &stats.TotalPoles,
@@ -45,22 +46,23 @@ func (r *Repository) GetTeamStats(ctx context.Context, team models.Team) (models
 
 func (r *Repository) GetDriverByName(ctx context.Context, name string) (models.Driver, error) {
 	var id uuid.UUID
-	var birthday, nationality string
+	var nationality string
+	var birthday time.Time
 	row := r.Pool.QueryRow(ctx,
-		"SELECT id, name, birthday, nationality FROM driver WHERE name like '%$1'", name)
+		"SELECT id, name, birthday, nationality FROM driver WHERE name like $1", name)
 	err := row.Scan(&id, &name, &birthday, &nationality)
 	if err != nil {
 		return models.Driver{}, err
 	}
 
-	return models.Driver{Id: id, Name: name, Birthday: birthday, Nationality: nationality}, nil
+	return models.Driver{Id: id, Name: name, Birthday: birthday.Format(time.DateOnly), Nationality: nationality}, nil
 }
 
 func (r *Repository) GetTeamByName(ctx context.Context, name string) (models.Team, error) {
 	var id uuid.UUID
 	var country string
 	row := r.Pool.QueryRow(ctx,
-		"SELECT id, name, country FROM team WHERE name like '%$1'", name)
+		"SELECT id, name, country FROM team WHERE name like $1", name)
 	err := row.Scan(&id, &name, &country)
 	if err != nil {
 		return models.Team{}, err
