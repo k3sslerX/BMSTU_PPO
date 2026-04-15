e84b148257e3b8ef462965f6ff5ce8a69112560b
k3ssler <jkessler365@mail.ru>
2026-04-15 11:17:03 +0300

    log init check

--- RacingGuru/cmd/main.go
+++ RacingGuru/cmd/main.go
@@ -7,6 +7,7 @@ import (
 	"RacingGuru/internal/repository"
 	"context"
 	"log"
+	"os"
 
 	"github.com/jackc/pgx/v4/pgxpool"
 )
@@ -14,6 +15,15 @@ import (
 func main() {
 	//dbURL := os.Getenv("DATABASE_URL")
 	//serverAddr := os.Getenv("SERVER_ADDR")
+	logFileName := os.Getenv("LOG_FILE")
+	logFile := os.Stdout
+	var err error
+	if logFileName != "" {
+		logFile, err = os.OpenFile(logFileName, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 	serverAddr := "localhost:8080"
 	dbURL := "postgresql://postgres:1337@localhost:5432/races"
 	pool, err := pgxpool.Connect(context.Background(), dbURL)
@@ -28,7 +38,7 @@ func main() {
 		return
 	}
 	handler := handlers.NewHandler(repo)
-	logger := log.Default()
+	logger := log.New(logFile, "", log.LstdFlags)
 	s := server.NewServer(serverAddr, handler.Routes(), logger)
 	log.Fatal(s.Run())
 }
